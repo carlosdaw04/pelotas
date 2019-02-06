9b66a40036e15af8649de57eec89d227d0238154
Julio P. López Bayón <jplbayon@gmail.com>
2019-02-06 14:08:12 +0100

    Se añade dificultad al juego añadiendo una pelota nueva si el usuario falla

--- src/PelotasLocas.java
+++ src/PelotasLocas.java
@@ -13,30 +13,38 @@ public class PelotasLocas extends Juego {
 	private static final int MAXVEL = 200;
 	private static final Random r = new Random();
 
+	private int tamRef;
+	private int minRadio;
+	private int maxRadio;
+	
 	private LinkedList<Pelota> pelotas;
 
 	public PelotasLocas(Lienzo lienzo, int numeroPelotas) {
 		super(lienzo);
 		pelotas = new LinkedList<Pelota>();
 
-		int tamRef = Math.max(lienzo.getWidth(), lienzo.getHeight());
-		int minRadio = (int) (tamRef * 0.02);
-		int maxRadio = (int) (tamRef * 0.10);
+		tamRef = Math.max(lienzo.getWidth(), lienzo.getHeight());
+		minRadio = (int) (tamRef * 0.02);
+		maxRadio = (int) (tamRef * 0.10);
 
 		for (int i = 0; i < numeroPelotas; i++) {
-			Color color = new Color(r.nextFloat(), r.nextFloat(), r.nextFloat());
-			int radio = r.nextInt(maxRadio - minRadio + 1) + minRadio;
-			int xymin = 10 + radio;
-			int xmax = lienzo.getWidth() - 10 - radio;
-			int ymax = lienzo.getHeight() - 10 - radio;
-			int x = r.nextInt(xmax - xymin + 1) + xymin;
-			int y = r.nextInt(ymax - xymin + 1) + xymin;
-			double dir = r.nextDouble() * 2 * Math.PI;
-			double vel = r.nextInt(MAXVEL - MINVEL + 1) + MINVEL;
-			pelotas.add(new Pelota(color, radio, x, y, dir, vel, lienzo.getSize()));
+			pelotas.add(nuevaPelota());
 		}
 	}
 
+	private Pelota nuevaPelota() {
+		Color color = new Color(r.nextFloat(), r.nextFloat(), r.nextFloat());
+		int radio = r.nextInt(maxRadio - minRadio + 1) + minRadio;
+		int xymin = 10 + radio;
+		int xmax = getLienzo().getWidth() - 10 - radio;
+		int ymax = getLienzo().getHeight() - 10 - radio;
+		int x = r.nextInt(xmax - xymin + 1) + xymin;
+		int y = r.nextInt(ymax - xymin + 1) + xymin;
+		double dir = r.nextDouble() * 2 * Math.PI;
+		double vel = r.nextInt(MAXVEL - MINVEL + 1) + MINVEL;
+		return new Pelota(color, radio, x, y, dir, vel, getLienzo().getSize());
+	}
+	
 	@Override
 	public void siguiente(long ns) {
 		synchronized (pelotas) {
@@ -54,6 +62,8 @@ public class PelotasLocas extends Juego {
 			Iterator<Pelota> i = pelotas.iterator();
 			while (i.hasNext())
 				i.next().paint(g);
+			g.setColor(Color.BLACK);
+			g.drawString("Número de pelotas: " + pelotas.size(), 50, 50);
 		}
 	}
 
@@ -83,10 +93,12 @@ public class PelotasLocas extends Juego {
 		int xr = e.getX();
 		int yr = e.getY();
 		int d = (int) Point2D.distance(xp, yp, xr, yr);
-		if (d < p.getRadio())
-			synchronized (pelotas) {
+		synchronized (pelotas) {
+			if (d < p.getRadio())
 				pelotas.remove(p);
-			}
+			else
+				pelotas.add(0, nuevaPelota());
+		}
 	}
 
 	@Override
