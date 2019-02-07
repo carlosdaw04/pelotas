9f4921b73365964ee5dbf6efe4e6e900d2a49b2d
Julio P. López Bayón <jplbayon@gmail.com>
2019-02-07 14:16:00 +0100

    Pelotas locas: las pelotas desaparecen lentamente

--- src/Pelota.java
+++ src/Pelota.java
@@ -1,6 +1,11 @@
+import java.awt.AlphaComposite;
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Composite;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
 
 public class Pelota {
 
@@ -16,6 +21,7 @@ public class Pelota {
 	private int ymax;
 	private int rx;
 	private int ry;
+	private AlphaComposite composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1);
 	
 	public Pelota(Color color, int radio, int x, int y, double d, double v, Dimension dim) {
 		this.color = color;
@@ -32,9 +38,9 @@ public class Pelota {
 		ry = dim.height + dim.height - diametro - diametro;
 	}
 	
-	public void mover(long t) {
-		double dx = t * vx / 1000000000d;
-		double dy = t * vy / 1000000000d;
+	public void mover(long lapso) {
+		double dx = lapso * vx / 1000000000d;
+		double dy = lapso * vy / 1000000000d;
 		x += dx;
 		y += dy;
 		if (x < 0) {
@@ -55,6 +61,16 @@ public class Pelota {
 		}
 	}
 	
+	public boolean desaparecer(long lapso) {
+		double alpha = composite.getAlpha();
+		if (alpha > 0) {
+			alpha -= (double) lapso / 2000000000d;
+			if (alpha >= 0)
+				composite = composite.derive((float) alpha);
+		}
+		return alpha <= 0;
+	}
+	
 	public int getX() {
 		return (int) x + radio;
 	}
@@ -72,6 +88,13 @@ public class Pelota {
 		g.fillOval((int) x, (int) y, diametro, diametro);
 		g.setColor(Color.BLACK);
 		g.drawOval((int) x, (int) y, diametro, diametro);
+	}
+	
+	public void paintDesaparecer(Graphics g) {
+		Composite aux = ((Graphics2D) g).getComposite();
+		((Graphics2D) g).setComposite(composite);
+		paint(g);
+		((Graphics2D) g).setComposite(aux);
 	}
 	
 }
--- src/PelotasLocas.java
+++ src/PelotasLocas.java
@@ -18,10 +18,12 @@ public class PelotasLocas extends Juego {
 	private int maxRadio;
 	
 	private LinkedList<Pelota> pelotas;
+	private ArrayList<Pelota> eliminadas;
 
 	public PelotasLocas(Lienzo lienzo, int numeroPelotas) {
 		super(lienzo);
 		pelotas = new LinkedList<Pelota>();
+		eliminadas = new ArrayList<Pelota>();
 
 		tamRef = Math.max(lienzo.getWidth(), lienzo.getHeight());
 		minRadio = (int) (tamRef * 0.02);
@@ -52,6 +54,15 @@ public class PelotasLocas extends Juego {
 			while (i.hasNext())
 				i.next().mover(ns);
 		}
+		synchronized (eliminadas) {
+			Iterator<Pelota> i = eliminadas.iterator();
+			while (i.hasNext()) {
+				Pelota p = i.next();
+				if (p.desaparecer(ns))
+					i.remove();
+			}
+		}
+		
 	}
 
 	@Override
@@ -60,10 +71,16 @@ public class PelotasLocas extends Juego {
 		g.fillRect(0, 0, getLienzo().getWidth(), getLienzo().getHeight());
 		synchronized (pelotas) {
 			Iterator<Pelota> i = pelotas.iterator();
-			while (i.hasNext())
+			while (i.hasNext()) {
 				i.next().paint(g);
+			}
 			g.setColor(Color.BLACK);
 			g.drawString("Número de pelotas: " + pelotas.size(), 50, 50);
+		}
+		synchronized (eliminadas) {
+			Iterator<Pelota> i = eliminadas.iterator();
+			while (i.hasNext()) 
+				i.next().paintDesaparecer(g);
 		}
 	}
 
@@ -94,8 +111,12 @@ public class PelotasLocas extends Juego {
 		int yr = e.getY();
 		int d = (int) Point2D.distance(xp, yp, xr, yr);
 		synchronized (pelotas) {
-			if (d < p.getRadio())
+			if (d < p.getRadio()) {
 				pelotas.remove(p);
+				synchronized (eliminadas) {
+					eliminadas.add(p);
+				}
+			}
 			else
 				pelotas.add(0, nuevaPelota());
 		}
